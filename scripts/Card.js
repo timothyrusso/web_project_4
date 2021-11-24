c166cbd5832d948cf7800f285ed4d26d7c804f7d
Timothy Russo <russo.t@cisalfasport.it>
2021-11-24 10:58:13 +0100

    fix: code refactoring

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,4 +1,32 @@
+const previewImageModalWindow = document.querySelector('.popup_type_preview'); // Let's find the preview modal in the DOM
+const previewImageElement = document.querySelector('.popup__preview-image');
+const captionImageElement = document.querySelector('.popup__caption');
 
+function closeEscButton(evt) { //---CLOSE THE POPUP WITH THE ESCAPE
+  const openedPopup = document.querySelector('.popup_opened');
+  if (evt.key == 'Escape' && openedPopup) {
+    closeModalWindow(openedPopup);
+  }
+}
+
+function openModalWindow(modal) { //---OPEN THE FORMS
+  disableModalButton(modal);
+  modal.classList.add('popup_opened');
+  addEscapeListener();
+}
+
+function closeModalWindow(modal) { //---CLOSE THE FORMS
+  modal.classList.remove('popup_opened');
+  removeEscapeListener();
+}
+
+function addEscapeListener() { //---ADD THE LISTENER FOR THE CLOSEESCBUTTON
+  document.addEventListener('keydown', closeEscButton);
+}
+
+function removeEscapeListener() { //REMOVE THE LISTENER FOR THE CLOSEESCBUTTON
+  document.removeEventListener('keydown', closeEscButton);
+}
 
 class Card {
 
@@ -31,9 +59,12 @@ class Card {
     return document.querySelector(this._cardSelector).content.querySelector('.card').cloneNode(true);
   }
 
-  getView() {
+  generateCard() {
     this._element = this._getTemplate();
     this._setEventListeners();
+
+    this._element.querySelector('.card__image').style.backgroundImage = `url(${this._link})`;
+    this._element.querySelector('.card__title').textContent = this._text;
   }
 }
 
